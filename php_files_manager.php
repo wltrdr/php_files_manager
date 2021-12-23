703dd6d9ae4be430db203cda84020b8e3848faf6
wltrdr <95063513+wltrdr@users.noreply.github.com>
2021-12-23 03:21:55 +0100

    Update php_files_manager.php

--- php_files_manager.php
+++ php_files_manager.php
@@ -12,6 +12,7 @@ session_start();
     protection scripts malicieux par url connue masquée
 
 [>>] affichage (fichier/dossier si accessible)
+gestion erreurs fatales [fatal=Unable to get server information]
 telecharger (fichier)
 supprimer partie historique si trop long
 type affichage elements
@@ -45,9 +46,41 @@ $password = 'admin';
 
 /* SECURITY */
 
+function get_user_ip()
+{
+    if(isset($_SERVER["HTTP_CF_CONNECTING_IP"]))
+    {
+        $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+        $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
+
+    $client = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    if(isset($_SERVER['REMOTE_ADDR']))
+        $remote = $_SERVER['REMOTE_ADDR'];
+    else
+        $remote = '';
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+        return $client;
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+        return $forward;
+    else
+        return $remote;
+}
+
 function sp_crypt($str)
 {
-    return sha1($_SERVER['REMOTE_ADDR'] . md5($str));
+    $remote_addr = get_user_ip();
+    if(isset($_SERVER['HTTP_USER_AGENT']))
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    else
+        $user_agent = '';
+
+    if(empty($remote_addr) && empty($user_agent))
+        exit('[fatal=Unable to get user information]');
+
+    return sha1($remote_addr . md5($str) . $user_agent);
 }
 
 function gencode($nb)
@@ -375,6 +408,9 @@ elseif(isset($_POST) && !empty($_POST))
             }
 
             $server_infos = server_infos();
+            if(!$server_infos)
+                exit('[fatal=Unable to get server information]');
+
             $script_path = $server_infos['server_root'] . $server_infos['dir'];
     
             $win_fs = true;
