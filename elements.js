5428e2fea3d6dc665060add9920ca16c4b621012
wltrdr <95063513+wltrdr@users.noreply.github.com>
2021-12-24 20:37:56 +0100

    Create elements.js

---
+++ elements.js
@@ -0,0 +1,142 @@
+function leftClickDir(dir)
+{
+    if(isOnMobile === false)
+        openDir(dir)
+    event.preventDefault()
+}
+
+function rightClickDir(name, pathEncoded, nameEncoded, urlEncoded, webUrl)
+{
+    menuDir(name, pathEncoded, nameEncoded, urlEncoded, webUrl)
+    event.preventDefault()
+}
+
+let timeClicDir = 0
+
+function startClicDir()
+{
+    if(isOnMobile === true)
+        timeClicDir = Date.now()
+    event.preventDefault()
+}
+
+function endClicDir(name, pathEncoded, nameEncoded, urlEncoded, webUrl)
+{
+    if(isOnMobile === true && timeClicDir !== 0)
+    {
+        if(Date.now() - timeClicDir > mslongClic)
+            menuDir(name, pathEncoded, nameEncoded, urlEncoded, webUrl)
+        else
+            openDir(urlEncoded)
+        timeClicDir = 0
+    }
+    event.preventDefault()
+}
+
+/* ADD ACTIONS */
+
+function newElement(type, name)
+{
+    if(name === "")
+        openBox("alert", "Error : <b>Name can't be empty</b>", "err")
+    else
+    {
+        ajaxRequest("POST", "", `${Date.now()}&new=${type}&name=${name}&dir=${currentPath}&token=${token}`, result => {
+            if(result === "created")
+                openDir(currentPath)
+            else
+            {
+                openDir(currentPath)
+                openBox("alert", "Error : <b>" + result + "</b>", "err")
+            }
+        })
+    }
+}
+
+function getUploadSizes(callback = false)
+{
+    if(uploadMaxFileSize === 0 || uploadMaxTotalSize === 0)
+    {
+        ajaxRequest("POST", "", `${Date.now()}&get_upload_sizes`, result => {
+            const found = result.match(/\[max_upload_sizes=([0-9]+)\|([0-9]+)\]/)
+            if(found)
+            {
+                uploadMaxFileSize = parseInt(found[1], 10)
+                uploadMaxTotalSize = parseInt(found[2], 10)
+                if(callback !== false)
+                {
+                    if(uploadMaxFileSize === 0 || uploadMaxTotalSize === 0)
+                        callback(false)
+                    else
+                        callback(true)
+                }
+            }
+            else if(callback !== false)
+                callback(false)
+        })
+    }
+    else if(callback !== false)
+        callback(true)
+}
+
+getUploadSizes()
+
+function uploadFiles()
+{
+    disableBodyPrevDef = true
+    setTimeout(() => {
+        inputUpload.click()
+        disableBodyPrevDef = false
+    }, delayMenuMs)
+}
+
+/* OTHERS ACTIONS */
+
+function downloadElement(path, name)
+{
+    window.open(`?${Date.now()}&download=${name}&dir=${path}&token=${token}`)
+}
+
+function renameElement(path, oldName, newName)
+{
+    if(newName === "")
+        openBox("alert", "Error : <b>Name can't be empty</b>", "err")
+    else
+    {
+        ajaxRequest("POST", "", `${Date.now()}&rename=${oldName}&dir=${path}&name=${newName}&token=${token}`, result => {
+            if(result === "renamed")
+                openDir(currentPath)
+            else
+            {
+                openDir(currentPath)
+                openBox("alert", "Error : <b>" + result + "</b>", "err")
+            }
+        })
+    }
+}
+
+function duplicateElement(path, name)
+{
+    ajaxRequest("POST", "", `${Date.now()}&duplicate=${name}&dir=${path}&token=${token}`, result => {
+        if(result === "duplicated")
+            openDir(currentPath)
+        else
+        {
+            openDir(currentPath)
+            openBox("alert", "Error : <b>" + result + "</b>", "err")
+        }
+    })
+}
+
+function deleteElement(path, name)
+{
+    ajaxRequest("POST", "", `${Date.now()}&delete=${name}&dir=${path}&token=${token}`, result => {
+        if(result === "deleted")
+            openDir(currentPath)
+        else
+        {
+            openDir(currentPath)
+            openBox("alert", "Error : <b>" + result + "</b>", "err")
+        }
+    })
+}
