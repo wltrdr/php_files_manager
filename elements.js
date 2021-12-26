3d00cb039ba3a567b30f24f6b8d9e597bf656bec
wltrdr <95063513+wltrdr@users.noreply.github.com>
2021-12-26 03:40:39 +0100

    Update elements.js

--- elements.js
+++ elements.js
@@ -14,7 +14,7 @@ function menuDir(name, pathEncoded, nameEncoded, urlEncoded, webUrl)
     <a onclick="openBox('path', 'Copy <b>ʿ${name}ʿ/</b> to :', null, inputPath => { copyElement('${pathEncoded}', '${nameEncoded}', inputPath) })">Copy to</a>
     <a onclick="openBox('path', 'Move <b>ʿ${name}ʿ/</b> to :', null, inputPath => { moveElement('${pathEncoded}', '${nameEncoded}', inputPath) })">Move to</a>
     <a onclick="openBox('confirm', 'Delete the directory <b>ʿ${name}/ʿ</b> ?', 'warn', () => { deleteElement('${pathEncoded}', '${nameEncoded}') })">Delete</a>
-    <a onclick="openBox('chmods', { name: '${name}', nameEncoded: '${nameEncoded}' })">Change chmods</a>
+    <a onclick="openBox('chmods', { name: '${name}/', nameEncoded: '${nameEncoded}' })">Change chmods</a>
     `, event)
 }
 
@@ -27,7 +27,7 @@ function menuFile(name, pathEncoded, nameEncoded, webUrl)
     openMenu(`<span>${name}</span>
     <a onclick="downloadElement('${pathEncoded}', '${nameEncoded}')">Download</a>
     ${webUrl}
-    <a onclick="openBox('edit', 'Edit <b>ʿ${name}ʿ</b> :', null, inputPath => { editElement('${pathEncoded}', '${nameEncoded}', inputPath) })">Edit</a>
+    <a onclick="openBox('edit', { name: '${name}', nameEncoded: '${nameEncoded}' })">Edit</a>
     <a onclick="openBox('prompt', { txt: 'Enter the new name for <b>ʿ${name}ʿ</b> :', value: '${name}' }, null, inputName => { renameElement('${pathEncoded}', '${nameEncoded}', inputName) })">Rename</a>
     <a onclick="duplicateElement('${pathEncoded}', '${nameEncoded}')">Duplicate</a>
     <a onclick="openBox('path', 'Copy <b>ʿ${name}ʿ</b> to :', null, inputPath => { copyElement('${pathEncoded}', '${nameEncoded}', inputPath) })">Copy to</a>
