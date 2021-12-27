83a06941d4fffe2e658a9e8da5464d7ac7ecaf60
wltrdr <95063513+wltrdr@users.noreply.github.com>
2021-12-27 05:18:40 +0100

    Update elements.js

--- elements.js
+++ elements.js
@@ -87,7 +87,7 @@ function getUploadSizes(callback = false)
 {
     if(uploadMaxFileSize === 0 || uploadMaxTotalSize === 0)
     {
-        ajaxRequest("POST", "", `${Date.now()}&get_upload_sizes`, result => {
+        ajaxRequest("GET", "", `${Date.now()}&get_upload_sizes`, result => {
             const found = result.match(/\[max_upload_sizes=([0-9]+)\|([0-9]+)\]/)
             if(found)
             {
