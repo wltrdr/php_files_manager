08c66bc1857d4cc45c8ce0093c33706beb59dc66
wltrdr <95063513+wltrdr@users.noreply.github.com>
2021-12-29 05:02:22 +0100

    Update files_edit.php

--- php/files_edit.php
+++ php/files_edit.php
@@ -45,7 +45,13 @@ function rm_full_dir($directory)
 		return false;
 }
 
-function copy_or_move($source, $dest, $move = false, $if_exists = 0) // $source CANNOT BE '.' OR EMPTY => USE '../current' INSTEAD // IF FILE EXISTS : $if_exists => 0 : DELETE OLD FILE | 1 : RENAME NEW FILE | 2 : RETURN FALSE
+/*
+$source CANNOT BE '.' OR EMPTY => USE '../current' INSTEAD
+$dest_file_exists : IF FILE EXISTS WHERE THE FILE MUST BE COPIED => 0 : RETURN FALSE | 1 : RENAME NEW FILE | 2 : DELETE OLD FILE
+$dest_dir_exists : IF DIR EXISTS WHERE THE FILE MUST BE COPIED => 0 : RETURN FALSE | 1 : RENAME NEW FILE | 2 : DELETE OLD DIR
+$fusion_dirs : IF DIR EXISTS WHERE THE DIR MUST BE COPIED => 0 : RETURN FALSE | 1 : RENAME NEW FILE | 2 : DELETE OLD DIR (ONLY FOR COPYING DIRS)
+*/
+function copy_or_move($source, $dest, $move = false, $dest_file_exists = 0, $dest_dir_exists = 0, $fusion_dirs = 0)
 {
 	$source = no_end_slash($source);
 	if(is_file($source))
@@ -62,28 +68,49 @@ function copy_or_move($source, $dest, $move = false, $if_exists = 0) // $source 
 			$dest .= '/';
 		$dest_exists = false;
 
+		$name_src_tmp = $name_dst_tmp = $source_name;
+
 		if(file_exists($dest . $source_name . $source_extension))
 		{
-			$new_name = $source_name;
-			$i = 1;
-			while(file_exists($dest . $new_name . " ($i)" . $source_extension))
-				$i++;
-			$new_name .= " ($i)";
-
-			if($source_path === $dest && $move === true)
+			if(is_dir($dest . $source_name . $source_extension))
+				$is_file = false;
+			elseif(is_file($dest . $source_name . $source_extension))
+				$is_file = true;
+			else
 				return false;
-			elseif($source_path === $dest)
+
+			if(($is_file === false && $dest_dir_exists === 0) || ($is_file === true && $dest_file_exists === 0))
+				return false;
+			elseif($is_file === false && $dest_dir_exists === 2)
 			{
-				$name_src_tmp = $source_name;
-				$name_dst_tmp = $new_name;
+				if(!rm_full_dir($dest . $source_name . $source_extension))
+					return false;
+			}
+			elseif($is_file === true && $dest_file_exists === 2)
+			{
+				if(!unlink($dest . $source_name . $source_extension))
+					return false;
 			}
 			else
 			{
-				$dest_exists = true;
-				$name_src_tmp = $name_dst_tmp = gencode(32);
-
-				if(!rename($source, $source_path . $name_src_tmp . $source_extension))
+				$new_name = $source_name;
+				$i = 1;
+				while(file_exists($dest . $new_name . " ($i)" . $source_extension))
+					$i++;
+				$new_name .= " ($i)";
+	
+				if($source_path === $dest && $move === true)
 					return false;
+				elseif($source_path === $dest)
+					$name_dst_tmp = $new_name;
+				else
+				{
+					$dest_exists = true;
+					$name_src_tmp = $name_dst_tmp = gencode(32);
+	
+					if(!rename($source, $source_path . $name_src_tmp . $source_extension))
+						return false;
+				}
 			}
 		}
 
@@ -133,13 +160,22 @@ function copy_or_move($source, $dest, $move = false, $if_exists = 0) // $source 
 					{
 						if(is_dir($source_path . $source_name . '/' . $entry))
 						{
-							if(!copy_or_move($source_path . $source_name . '/' . $entry, $dest . $new_name, $move))
+							if(!copy_or_move($source_path . $source_name . '/' . $entry, $dest . $new_name, $move, $dest_file_exists, $dest_dir_exists, $fusion_dirs))
 								return false;
 						}
 						elseif(is_file($source_path . $source_name . '/' . $entry))
 						{
 							if(!copy($source_path . $source_name . '/' . $entry, $dest . $new_name . '/' . $entry))
 								return false;
+/*
+							if(copy($source_path . $source_name . '/' . $entry, $dest . $new_name . '/' . $entry))
+							{
+								if($move === true && !unlink($source_path . $source_name . '/' . $entry))
+									return false;
+							}
+							else
+								return false;
+*/
 						}
 						else
 							return false;
@@ -150,6 +186,17 @@ function copy_or_move($source, $dest, $move = false, $if_exists = 0) // $source 
 					return false;
 				else
 					return true;
+/*
+				if($move === true)
+				{
+					if(rmdir($source_path . $source_name))
+						return true;
+					else
+						return false;
+				}
+				else
+					return true;
+*/
 			}
 			else
 				return false;
