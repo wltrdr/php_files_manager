56052e59acb8f3f9ef8995f5fca9eafc80a1d150
wltrdr <95063513+wltrdr@users.noreply.github.com>
2022-01-10 08:39:21 +0100

    Update elements.js

--- js/elements.js
+++ js/elements.js
@@ -52,21 +52,24 @@ function menuFile(name, pathEncoded, nameEncoded, webUrl, isLink = false) {
 		`, event)
 }
 
+function checkReqRep(request, wish) {
+	ajaxRequest("POST", "", request, result => {
+		if(result === wish)
+			openDir(currentPath, true)
+		else {
+			openDir(currentPath, true)
+			openBox("alert", "Error : <b>" + result + "</b>", "err")
+		}
+	})
+}
+
 /* ADD ACTIONS */
 
 function newElement(type, name) {
 	if(name === "")
 		openBox("alert", "Error : <b>Name can't be empty</b>", "err")
-	else {
-		ajaxRequest("POST", "", `${Date.now()}&new=${type}&dir=${currentPath}&name=${name}&token=${token}`, result => {
-			if(result === "created")
-				openDir(currentPath, true)
-			else {
-				openDir(currentPath, true)
-				openBox("alert", "Error : <b>" + result + "</b>", "err")
-			}
-		})
-	}
+	else 
+		checkReqRep(`${Date.now()}&new=${type}&dir=${currentPath}&name=${name}&token=${token}`, "created")
 }
 
 /* OTHER ACTIONS */
@@ -78,58 +81,33 @@ function downloadElement(pathEncoded, nameEncoded) {
 function renameElement(pathEncoded, oldName, newName) {
 	if(newName === "")
 		openBox("alert", "Error : <b>Name can't be empty</b>", "err")
-	else {
-		ajaxRequest("POST", "", `${Date.now()}&rename=${oldName}&dir=${pathEncoded}&name=${newName}&token=${token}`, result => {
-			if(result === "renamed")
-				openDir(currentPath, true)
-			else {
-				openDir(currentPath, true)
-				openBox("alert", "Error : <b>" + result + "</b>", "err")
-			}
-		})
-	}
+	else
+		checkReqRep(`${Date.now()}&rename=${oldName}&dir=${pathEncoded}&name=${newName}&token=${token}`, "renamed")
 }
 
 function duplicateElement(pathEncoded, nameEncoded) {
-	ajaxRequest("POST", "", `${Date.now()}&duplicate=${nameEncoded}&dir=${pathEncoded}&path=${pathEncoded}&token=${token}`, result => {
-		if(result === "duplicated")
-			openDir(currentPath, true)
-		else {
-			openDir(currentPath, true)
-			openBox("alert", "Error : <b>" + result + "</b>", "err")
-		}
-	})
+	checkReqRep(`${Date.now()}&duplicate=${nameEncoded}&dir=${pathEncoded}&path=${pathEncoded}&token=${token}`, "duplicated")
 }
 
 function copyElement(pathEncoded, nameEncoded, newPath) {
-	ajaxRequest("POST", "", `${Date.now()}&copy=${nameEncoded}&dir=${pathEncoded}&path=${newPath}&if_exists=${typeCopyMoveExists}&token=${token}`, result => {
-		if(result === "copied")
-			openDir(currentPath, true)
-		else {
-			openDir(currentPath, true)
-			openBox("alert", "Error : <b>" + result + "</b>", "err")
-		}
-	})
+	checkReqRep(`${Date.now()}&copy=${nameEncoded}&dir=${pathEncoded}&path=${newPath}&if_exists=${typeCopyMoveExists}&token=${token}`, "copied")
 }
 
 function moveElement(pathEncoded, nameEncoded, newPath) {
-	ajaxRequest("POST", "", `${Date.now()}&move=${nameEncoded}&dir=${pathEncoded}&path=${newPath}&if_exists=${typeCopyMoveExists}&token=${token}`, result => {
-		if(result === "moved")
-			openDir(currentPath, true)
-		else {
-			openDir(currentPath, true)
-			openBox("alert", "Error : <b>" + result + "</b>", "err")
-		}
-	})
+	checkReqRep(`${Date.now()}&move=${nameEncoded}&dir=${pathEncoded}&path=${newPath}&if_exists=${typeCopyMoveExists}&token=${token}`, "moved")
 }
 
 function deleteElement(pathEncoded, nameEncoded) {
-	ajaxRequest("POST", "", `${Date.now()}&delete=${nameEncoded}&dir=${pathEncoded}&token=${token}`, result => {
-		if(result === "deleted")
-			openDir(currentPath, true)
-		else {
-			openDir(currentPath, true)
-			openBox("alert", "Error : <b>" + result + "</b>", "err")
-		}
+	checkReqRep(`${Date.now()}&delete=${nameEncoded}&dir=${pathEncoded}&token=${token}`, "deleted")
+}
+
+function moveMultiple(pathEncoded) {
+	let strSelecteds = ""
+	selectedElements.forEach(element => {
+		if(currentPath !== ".")
+			strSelecteds += currentPath
+		strSelecteds += element.nameEncoded + "%7C%7C%7C"
 	})
+	strSelecteds = strSelecteds.substring(0, strSelecteds.length - 9)
+	checkReqRep(`${Date.now()}&move_multiple=${strSelecteds}&dir=${pathEncoded}&if_exists=${typeCopyMoveExists}&token=${token}`, "moveds")
 }
