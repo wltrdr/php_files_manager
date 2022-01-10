9deb673cd0ba7dc502d889cdbfd71f11e21946fa
wltrdr <95063513+wltrdr@users.noreply.github.com>
2022-01-10 14:18:45 +0100

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -249,3 +249,14 @@ function removeObjsInArr(arr, val, param, insensible = false)
         }
     }
 }
+
+function checkReqRep(request, wish, disableFocus = true) {
+	ajaxRequest("POST", "", request, result => {
+		if(result === wish)
+			openDir(currentPath, disableFocus)
+		else {
+			openDir(currentPath, disableFocus)
+			openBox("alert", "Error : <b>" + result + "</b>", "err")
+		}
+	})
+}
