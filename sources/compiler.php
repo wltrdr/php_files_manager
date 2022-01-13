895a88623ea9ac05e70333bfa1f0be98bd086937
wltrdr <95063513+wltrdr@users.noreply.github.com>
2022-01-13 13:47:49 +0100

    Update compiler.php

--- sources/compiler.php
+++ sources/compiler.php
@@ -1,18 +1,18 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
 $file = file_get_contents('php_files_manager.src.php');
-function m_stripslashes($str) {
+function addsomeslashes($str) {
 	return str_replace('\'', '\\\'', str_replace('\\', '\\\\', $str));
 }
 
 /* CSS, JS && TRASH.HTML */
 preg_match_all('#exit\(file_get_contents\(\'([^\']+)\'\)\);#', $file, $matches, PREG_SET_ORDER);
 foreach($matches as $matche) {
-	$file = str_replace($matche[0], 'exit(\'' . m_stripslashes(file_get_contents($matche[1])) . '\');', $file);
+	$file = str_replace($matche[0], 'exit(\'' . addsomeslashes(file_get_contents($matche[1])) . '\');', $file);
 }
 
 /* TEMPLATE.HTML */
-$file = str_replace('\\\' . version_script . \\\'', '\' . version_script . \'', str_replace('str_replace(\'\\\' . version_script . \\\'\', version_script, file_get_contents(\'template/template.html\'))', '\'' . m_stripslashes(file_get_contents('template/template.html')) . '\'', $file));
+$file = str_replace('\\\' . version_script . \\\'', '\' . version_script . \'', str_replace('str_replace(\'\\\' . version_script . \\\'\', version_script, file_get_contents(\'template/template.html\'))', '\'' . addsomeslashes(file_get_contents('template/template.html')) . '\'', $file));
 
 /* INCLUDES */
 preg_match_all('#include\(\'([^\']+)\'\);#', $file, $matches, PREG_SET_ORDER);
