02e6f7f34c611c5782aa34a572da80adbd141139
wltrdr <95063513+wltrdr@users.noreply.github.com>
2022-01-14 19:34:59 +0100

    Update events.js

--- sources/js/events.js
+++ sources/js/events.js
@@ -539,9 +539,9 @@ ajaxRequest("GET", urlRawGithub, "", result => {
 				console.log("No Update available !")
 		}
 		else
-			console.log("%cError : %cUnable to access script version", "color: red;", "color: auto;")
+			console.log("%c Error : %cUnable to access script version", "color: red;", "color: auto;")
 	}
 	else
-		console.log("%cError : %cUnable to access new script version", "color: red;", "color: auto;")
+		console.log("%c Error : %cUnable to access new script version", "color: red;", "color: auto;")
 
 }, true, true)
